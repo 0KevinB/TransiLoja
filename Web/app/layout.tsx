39e99c356a64de9e822e3444ac0b168e239f8e4f
Kevin <kabarrazueta@utpl.edu.ec>
2025-04-23 09:57:45 -0500

    Inicio del proyecto web

---
+++ Web/app/layout.tsx
@@ -0,0 +1,30 @@
+import type React from "react"
+import "./globals.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "TransiLoja - Aplicación de Transporte Público Loja",
+  description:
+    "Una solución tecnológica pensada para facilitar el acceso a la información del transporte urbano en la ciudad de Loja, Ecuador.",
+    generator: 'Carolina Alvarado, Kevin Barrazueta'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="es" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
